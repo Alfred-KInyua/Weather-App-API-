1c67987cbfdb0e289433e66c5dd080b46e2fb54f
Alfred-KInyua <alfkinyua@gmail.com>
2025-10-09 09:41:19 +0300

    update default city to nairobi

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const API_KEY = "9d2402c9907e4accdd47a8578b8bb50c";
 const base_url = "https://api.openweathermap.org/data/2.5";
 
-const defaultCity = "London";
-const countryCode = "GB";
+const defaultCity = "Nairobi";
+const countryCode = "KE";
 const units = "metric";
 
 const cityInput = document.getElementById("city-input");
